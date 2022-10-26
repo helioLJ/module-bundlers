ee48ba48b0c2045024536ca927fd9e1f9cd86025
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2022-10-26 09:37:46 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'awesome.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        // rules = match files to loaders
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 9000
+    }
+};
